bb2fbb0df6d32fe013bda69f7bf04a54cd8ec3f9
Harun Abukar <harun.abukar@kcl.ac.uk>
2023-10-18 12:19:22 +0100

    attemped tasks

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,13 @@
 """Forms of the project."""
 
+from django import forms
+from .forms import ThingForm
 # Create your forms here.
+
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ["name", "description", "quantity"]
+        widgets = {"description": forms.TextArea(),
+                    "quantity": forms.NumberInput()}
+
--- things/views.py
+++ things/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import ThingForm
 
 def home(request):
     return render(request, 'home.html', {'form': form})
